6cf530cfe72630d193b8008523cc1b824a9f9eac
Andre Lewis <andre@fromberkeleywithlove.com>
2026-02-03 10:37:09 -0800

    Added a folder view

--- MainSplit.py
+++ MainSplit.py
@@ -1,7 +1,9 @@
 import sys
+import os
 import numpy as np
-from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog
-from PyQt6.QtCore import QUrl
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QFileSystemModel
+from PyQt6.QtCore import QUrl, QDir, QModelIndex
 from PyQt6.QtGui import QKeyEvent
 from PyQt6.QtMultimedia import (QMediaPlayer, QAudioOutput, QAudioDecoder, 
                                  QAudioBuffer, QAudioFormat)
@@ -10,12 +12,24 @@ from PyQt6 import uic
 class AudioApp(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
-        # Load the UI file
         uic.loadUi("player.ui", self)
 
-        # Access widgets defined in the UI file by their objectName
-        # self.waveform and self.btnMain are automatically created attributes
+        # 1. Setup File System Model
+        self.model = QFileSystemModel()
+        home_path = QDir.homePath()
+        self.model.setRootPath(home_path)
         
+        # Filter for audio files only
+        self.model.setNameFilters(["*.wav", "*.mp3", "*.flac", "*.m4a", "*.ogg"])
+        self.model.setNameFilterDisables(False) # Hide non-matching files
+
+        # Setup TreeView
+        self.treeView.setModel(self.model)
+        self.treeView.setRootIndex(self.model.index(home_path))
+        self.treeView.setColumnWidth(0, 250) # Make filename column wider
+        self.treeView.doubleClicked.connect(self.on_file_selected)
+
+        # 2. Multimedia Engine
         self.player = QMediaPlayer()
         self.audio_output = QAudioOutput()
         self.player.setAudioOutput(self.audio_output)
@@ -25,33 +39,31 @@ class AudioApp(QMainWindow):
         self.decoder.finished.connect(self._on_decoder_finished)
         
         self.accumulated_data: list[np.ndarray] = []
-        self.file_ready = False
+        self.current_path = ""
 
-        # Connect signals to logic
-        self.btnMain.clicked.connect(self.handle_interaction)
+        # 3. Connections
+        self.btnMain.clicked.connect(self.toggle_playback)
         self.player.playbackStateChanged.connect(self._on_state_changed)
         self.player.mediaStatusChanged.connect(self._on_status_changed)
 
-    def handle_interaction(self) -> None:
-        if not self.file_ready:
-            self.load_file()
-        elif self.player.playbackState() == QMediaPlayer.PlaybackState.PlayingState:
-            self.player.stop()
-        else:
-            self.player.play()
+    def on_file_selected(self, index: QModelIndex) -> None:
+        path = self.model.filePath(index)
+        if not os.path.isdir(path):
+            self.load_and_play(path)
 
-    def load_file(self) -> None:
-        path, _ = QFileDialog.getOpenFileName(self, "Open Audio", "", "Audio (*.wav *.mp3 *.flac)")
-        if path:
-            self.file_ready = False
-            self.accumulated_data = []
-            self.btnMain.setText("Decoding...")
-            self.btnMain.setEnabled(False)
-            
-            url = QUrl.fromLocalFile(path)
-            self.decoder.setSource(url)
-            self.player.setSource(url)
-            self.decoder.start()
+    def load_and_play(self, path: str) -> None:
+        # One-shot logic: Stop existing playback
+        self.player.stop()
+        self.current_path = path
+        self.accumulated_data = []
+        
+        self.btnMain.setText("Decoding...")
+        self.btnMain.setEnabled(False)
+        
+        url = QUrl.fromLocalFile(path)
+        self.decoder.setSource(url)
+        self.player.setSource(url)
+        self.decoder.start()
 
     def _process_buffer(self) -> None:
         buf: QAudioBuffer = self.decoder.read()
@@ -71,12 +83,21 @@ class AudioApp(QMainWindow):
     def _on_decoder_finished(self) -> None:
         if self.accumulated_data:
             self.waveform.set_samples(np.concatenate(self.accumulated_data))
-        self.file_ready = True
+        
         self.btnMain.setEnabled(True)
-        self.btnMain.setText("Play")
+        self.player.play() # Auto-play once decoded
+
+    def toggle_playback(self) -> None:
+        if self.player.playbackState() == QMediaPlayer.PlaybackState.PlayingState:
+            self.player.stop()
+        else:
+            self.player.play()
 
     def _on_state_changed(self, state: QMediaPlayer.PlaybackState) -> None:
-        self.btnMain.setText("Stop (Key to Interrupt)" if state == QMediaPlayer.PlaybackState.PlayingState else "Play")
+        if state == QMediaPlayer.PlaybackState.PlayingState:
+            self.btnMain.setText("Stop (Key to Interrupt)")
+        else:
+            self.btnMain.setText("Play Selection")
 
     def _on_status_changed(self, status: QMediaPlayer.MediaStatus) -> None:
         if status == QMediaPlayer.MediaStatus.EndOfMedia:
